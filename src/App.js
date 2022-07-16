b96e2572d9ac7900a9be72e75fbc2ab52ab20545
HeraTheYorkie <nikolina.pavic@gmail.com>
2022-07-16 22:11:36 +0200

    Add avatar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Avatar } from "./components/Avatar";
 import { MessageText } from "./components/MessageText";
 
 
 function App() {
-  return <MessageText displayName="HeraTheYorkie" time="20:36">
-Who let the dog out? wuf, wuf, wuf 
-  </MessageText>;
+  return <div>
+    <Avatar text="HP" backgroundColor="yellow" />
+    <MessageText displayName="HeraTheYorkie" time="20:36">
+      Who let the dog out? wuf, wuf, wuf 
+    </MessageText>
+  </div> 
 }
 
 export default App;
