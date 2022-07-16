2ef5c52c553c3044c4a4d276ab6ef52844462b52
HeraTheYorkie <nikolina.pavic@gmail.com>
2022-07-16 20:42:12 +0200

    Add message metadata

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { MessageForm } from "./components/MessageForm";
+import { MessageMetadata } from "./components/MessageMetadata/MessageMetadata.component";
 import { JoinPage } from "./pages/JoinPage";
 
 function App() {
-  return <MessageForm />;
+  return <MessageMetadata displayName="HeraTheYorkie" time="20:36" />;
 }
 
 export default App;
--- src/components/MessageMetadata/MessageMetadata.component.jsx
+++ src/components/MessageMetadata/MessageMetadata.component.jsx
@@ -1,14 +1,14 @@
-import "./MessageMetadat.styles.scss";
+import "./MessageMetadata.styles.scss";
 
 
 
 export function MessageMetadata(props) {
   return (
-    <span className="message-metadat">
+    <span className="message-metadata">
         <span className="message-metadata__display-name">
             {props.displayName}
         </span>
-        <span className="message-metadat__time">
+        <span className="message-metadata__time">
             {props.time}
         </span>
     </span>
