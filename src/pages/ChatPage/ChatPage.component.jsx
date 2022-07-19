ae67e709e81a0c20d6280253d81d4098640c7e2c
HeraTheYorkie <nikolina.pavic@gmail.com>
2022-07-19 18:35:43 +0200

    Set up Scaledrone connection

--- src/pages/ChatPage/ChatPage.component.jsx
+++ src/pages/ChatPage/ChatPage.component.jsx
@@ -1,23 +1,55 @@
 import "./ChatPage.styles.scss";
 
 import { Message as MessageModel } from "../../models/Message";
-import { Message } from "../../components/Message"
-import { MessageForm } from "../../components/MessageForm"
+import { Message } from "../../components/Message";
+import { MessageForm } from "../../components/MessageForm";
 import { useUser } from "../../contexts/UserContext";
 import { useState } from "react";
+import { useEffect } from "react";
 
-export function ChatPage (props) {
-    const { user } = useUser();
-    const [state, setState] = useState([]);
+export function ChatPage() {
+  const { user } = useUser();
+  const [state, setState] = useState([]);
+  const [drone, setDrone] = useState(null);
 
-    const sendMessage = (formState) => {    const message = new MessageModel({
-        messageText: formState.message,
-        user,
+  const sendMessage = (formState) => {
+    const message = new MessageModel({
+      messageText: formState.message,
+      user,
+    });
+
+    if (drone !== null) {
+      drone.publish({
+        room: 'chat',
+        message: message
       });
-  
-      setState((state) => [ ...state, message ]);
     }
 
+    setState((state) => [ ...state, message ]);
+  }
+
+  useEffect(() => {
+    if (drone !== null) return;
+    // eslint-disable-next-line no-undef
+    setDrone(new Scaledrone('aC1vqpO2aEavkoXU'));
+  }, [drone, setDrone]);
+
+  useEffect(() => {
+    if (drone === null) return;
+
+    const room = drone.subscribe('chat');
+
+    room.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log('Connected to room');
+    });
+  
+    room.on('message', message => {
+      console.log('Message received', message);
+    });
+  }, [drone]);
 
   const messageItems = state.map((message, index) => (
     <div key={index} className="chat-page__message-list-item">
@@ -32,19 +64,15 @@ export function ChatPage (props) {
     </div>
   ));
 
-    return (
-        <div className="chat-page">
-            <div className="chat-page__title">Chat with friends</div>
-            <div className="chat-page__message-list">
-                {messageItems}
-            </div>
-            <div className="chat-page__form">
-                <MessageForm 
-                onSend={sendMessage}
-                 />
-            </div>
-        </div> 
-
-    );
+  return (
+    <div className="chat-page">
+      <div className="chat-page__title">Chat with friends</div>
+      <div className="chat-page__message-list">
+        {messageItems}
+      </div>
+      <div className="chat-page__form">
+        <MessageForm onSend={sendMessage} />
+      </div>
+    </div>
+  );
 }
-
