8943eec9bc737fcad34a00f6103c28ef76e70279
HeraTheYorkie <nikolina.pavic@gmail.com>
2022-07-20 20:44:32 +0200

    Add side

--- src/components/Message/Message.component.jsx
+++ src/components/Message/Message.component.jsx
@@ -1,5 +1,6 @@
 import "./Message.styles.scss";
 
+import { useUser } from "../../contexts/UserContext";
 import { Avatar } from "../Avatar";
 import { MessageText } from "../MessageText";
 import { useEffect } from "react";
@@ -7,13 +8,14 @@ import { useRef } from "react";
 
 export function Message(props) {
   const ref = useRef();
+  const user = useUser();
 
   useEffect(() => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
   }, []);
 
   return (
-    <div ref={ref} className="message">
+    <div ref={ref} className={`message ${user.user.displayName === props.displayName ? 'my-messages' : ''}`}>
     <div className="message">
       <div className="message__avatar">
         <Avatar
